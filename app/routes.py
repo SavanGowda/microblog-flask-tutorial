a0fab3628b6bb9883a42c02178884ebbc2782d17
U733584 <savan.rangegowda@lhind.dlh.de>
2020-02-26 16:27:53 +0100

    add html files

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,33 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World! from Savan"
+    #return "Hello World! from Savan"
+
+    user = {'username' : 'Viewers'}
+
+    posts = [
+        {
+            'author': {'username':'Jon'},
+            'body': 'I am the Commander of the Nightswatch!'
+        },
+
+        {
+            'author': {'username':'Nedd'},
+            'body': 'One who passes the sentence, must swing the sword!'
+        },
+
+        {
+            'author': {'username':'Ygritte'},
+            'body': 'You know nothing Jon Snow!'
+        },
+
+        {
+            'author': {'username':'Cersi'},
+            'body': 'The Iron throne is mine and I shall keep it!'
+        }
+    ]
+
+    return render_template('index.html', title = 'Home', user=user, posts = posts)
